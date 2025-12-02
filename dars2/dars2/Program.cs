6b4e742409b2fdff08d1910c58d300f1ab73a14a
juratovorif-web <juratovorif@gmail.com>
2025-12-02 23:23:12 +0500

    uyiwi

--- dars2/dars2/Program.cs
+++ dars2/dars2/Program.cs
@@ -4,10 +4,14 @@
     {
         static void Main(string[] args)
         {
-            /*var dayValue = int.Parse(Console.ReadLine());
+            /*
+             * var dayValue = int.Parse(Console.ReadLine());
             var timeValue = int.Parse(Console.ReadLine());
             var res = $"{ dayValue * 24 + timeValue}  >>> soat ";
-            Console.WriteLine(res); */
+            Console.WriteLine(res); 
+            */
+
+            /*
             Console.Write("patriki puli enter : ");
             var patrikCash = int.Parse(Console.ReadLine());
             Console.Write("juliyani puli enter : ");
@@ -15,6 +19,15 @@
 
             var res = $"patrik {patrikCash -  juliyaCash} som pul bersa julyani puli tenglawadi";
             Console.WriteLine(res);
+            */
+
+            Console.Write("akmalni daqiqasga newta belgi yozad :");
+            var akmalEnterStringNumbers = int.Parse(Console.ReadLine());
+            var belgilar = akmalEnterStringNumbers * 2.5;
+            var soz = belgilar / 5;
+             
+           
+            Console.WriteLine($"{soz} ta yozdi");
         }
     }
 }
