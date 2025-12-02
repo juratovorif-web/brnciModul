49f73524727b1df451cff0e5040657dc66f169cf
juratovorif-web <juratovorif@gmail.com>
2025-12-02 22:56:11 +0500

    15 msol

--- dars2/dars2/Program.cs
+++ dars2/dars2/Program.cs
@@ -4,9 +4,16 @@
     {
         static void Main(string[] args)
         {
-            var dayValue = int.Parse(Console.ReadLine());
+            /*var dayValue = int.Parse(Console.ReadLine());
             var timeValue = int.Parse(Console.ReadLine());
             var res = $"{ dayValue * 24 + timeValue}  >>> soat ";
+            Console.WriteLine(res); */
+            Console.Write("patriki puli enter : ");
+            var patrikCash = int.Parse(Console.ReadLine());
+            Console.Write("juliyani puli enter : ");
+            var juliyaCash = int.Parse(Console.ReadLine());
+
+            var res = $"patrik {patrikCash -  juliyaCash} som pul bersa julyani puli tenglawadi";
             Console.WriteLine(res);
         }
     }
