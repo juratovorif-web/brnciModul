8eb446fc3a48767a15a19962601c891232654de3
juratovorif-web <juratovorif@gmail.com>
2025-12-06 22:07:20 +0500

    add replaceLetters metod

--- darss4/Program.cs
+++ darss4/Program.cs
@@ -4,17 +4,29 @@
     {
         static void Main(string[] args)
         {
-            var firstNumber = int.Parse(Console.ReadLine());
-            var secondNumber = int.Parse(Console.ReadLine());
+            string str = "Foundation";
 
-            Console.WriteLine(QowbCqarw(firstNumber, secondNumber));
+            var res = str.Substring(1, str.Length - 3);
+            Console.WriteLine(res);
         }
 
-
+    
         static int QowbCqarw(int a, int b)
         {
-            //njn
             return a + b;
+        }
+
+        static string ReplaceLetters(string str)
+        {
+            var res = string.Empty;
+            foreach (char i in str)
+            {
+                if (i == 'x') res += 'y';
+                else if (i == 'y') res += 'z';
+                else res += i;
+            }
+
+            return res;
         }
     }
 
